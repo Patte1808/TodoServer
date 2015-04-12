2d0ffe0da00dd8fdd9452b69c23ba09066fd9c46
Patte1808 <Patrick.Voss1808@gmail.com>
2015-04-12 12:26:22 +0200

    Add post route for todo

--- server.js
+++ server.js
@@ -1,4 +1,36 @@
 var express = require('express');
 var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
+
+var Todo = require('./models/todo');
+
+var app = express();
+var router = express.Router();
+var port = process.env.PORT || 3000;
 
 mongoose.connect('mongodb://localhost:27017/todo');
+
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+var todoRoute = router.route('/todo');
+
+todoRoute.post(function(req, res) {
+  var todo = new Todo();
+
+  todo.title = req.body.title;
+  todo.completed = req.body.completed;
+
+  todo.save(function(err) {
+    if(err)
+      res.send(err)
+
+    res.json({data:todo});
+  });
+});
+
+app.use('/api', router);
+
+app.listen(port);
+console.log('Running on port ' + port);
